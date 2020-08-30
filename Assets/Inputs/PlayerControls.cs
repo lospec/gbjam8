33840038e373e95bb8852c140465177ebb85836b
Pillsgood <arashm35@gmail.com>
2020-08-30 16:08:10 +0800

    use InputActions for player movement; rework physics;

---
+++ Assets/Inputs/PlayerControls.cs
@@ -0,0 +1,258 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace Inputs
+{
+    public class @PlayerControls : IInputActionCollection, IDisposable
+    {
+        public InputActionAsset asset { get; }
+        public @PlayerControls()
+        {
+            asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""9bddb279-de90-483f-b5fa-a39db6a9d2a7"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""cc1e027c-93be-41dd-8a0b-352eac4cfff9"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Primary"",
+                    ""type"": ""Button"",
+                    ""id"": ""db4df0bf-90dd-4aff-a4cf-c2fc5537beeb"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Secondary"",
+                    ""type"": ""Button"",
+                    ""id"": ""817b7f04-03c1-4771-960d-a34c1925e375"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""46ccb1eb-6208-4f81-a310-709ac1975aae"",
+                    ""path"": ""<Keyboard>/x"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""Primary"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3818f9e7-a78e-4ca8-bc74-665a9ad245a5"",
+                    ""path"": ""<Keyboard>/c"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""Secondary"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""ArrowKeys"",
+                    ""id"": ""22dc4c55-dbd2-4500-869f-dbed3ef56571"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""393e92e9-aa95-4e19-9116-0ff73fad2415"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""d47e8e9a-10bb-420f-b9e6-90cda702bf68"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""4ab42808-4412-4916-b2c5-6a3d8d4f25cb"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""fba2e9ab-b65b-40f7-98ec-ede5217a46f2"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""keyboard"",
+            ""bindingGroup"": ""keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+            // Player
+            m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+            m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+            m_Player_Primary = m_Player.FindAction("Primary", throwIfNotFound: true);
+            m_Player_Secondary = m_Player.FindAction("Secondary", throwIfNotFound: true);
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(asset);
+        }
+
+        public InputBinding? bindingMask
+        {
+            get => asset.bindingMask;
+            set => asset.bindingMask = value;
+        }
+
+        public ReadOnlyArray<InputDevice>? devices
+        {
+            get => asset.devices;
+            set => asset.devices = value;
+        }
+
+        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+        public bool Contains(InputAction action)
+        {
+            return asset.Contains(action);
+        }
+
+        public IEnumerator<InputAction> GetEnumerator()
+        {
+            return asset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enable()
+        {
+            asset.Enable();
+        }
+
+        public void Disable()
+        {
+            asset.Disable();
+        }
+
+        // Player
+        private readonly InputActionMap m_Player;
+        private IPlayerActions m_PlayerActionsCallbackInterface;
+        private readonly InputAction m_Player_Movement;
+        private readonly InputAction m_Player_Primary;
+        private readonly InputAction m_Player_Secondary;
+        public struct PlayerActions
+        {
+            private @PlayerControls m_Wrapper;
+            public PlayerActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Movement => m_Wrapper.m_Player_Movement;
+            public InputAction @Primary => m_Wrapper.m_Player_Primary;
+            public InputAction @Secondary => m_Wrapper.m_Player_Secondary;
+            public InputActionMap Get() { return m_Wrapper.m_Player; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+            public void SetCallbacks(IPlayerActions instance)
+            {
+                if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+                {
+                    @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                    @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                    @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                    @Primary.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPrimary;
+                    @Primary.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPrimary;
+                    @Primary.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPrimary;
+                    @Secondary.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSecondary;
+                    @Secondary.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSecondary;
+                    @Secondary.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSecondary;
+                }
+                m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @Movement.started += instance.OnMovement;
+                    @Movement.performed += instance.OnMovement;
+                    @Movement.canceled += instance.OnMovement;
+                    @Primary.started += instance.OnPrimary;
+                    @Primary.performed += instance.OnPrimary;
+                    @Primary.canceled += instance.OnPrimary;
+                    @Secondary.started += instance.OnSecondary;
+                    @Secondary.performed += instance.OnSecondary;
+                    @Secondary.canceled += instance.OnSecondary;
+                }
+            }
+        }
+        public PlayerActions @Player => new PlayerActions(this);
+        private int m_keyboardSchemeIndex = -1;
+        public InputControlScheme keyboardScheme
+        {
+            get
+            {
+                if (m_keyboardSchemeIndex == -1) m_keyboardSchemeIndex = asset.FindControlSchemeIndex("keyboard");
+                return asset.controlSchemes[m_keyboardSchemeIndex];
+            }
+        }
+        public interface IPlayerActions
+        {
+            void OnMovement(InputAction.CallbackContext context);
+            void OnPrimary(InputAction.CallbackContext context);
+            void OnSecondary(InputAction.CallbackContext context);
+        }
+    }
+}
--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -1,101 +1,105 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
+using Inputs;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Player
 {
-    public class CharacterController : MonoBehaviour
+    [RequireComponent(typeof(SpriteRenderer),
+        typeof(Rigidbody),
+        typeof(PlayerInput))]
+    public class CharacterController : MonoBehaviour, PlayerControls.IPlayerActions
     {
-        public Direction direction = Direction.E;
-        public LayerMask
-            floorLayer; //Use this to set which layers will be checked for colliders to see whether the character is grounded.
+        private const float MoveSpeedModifier = 10f;
 
-        //Create two empties and position them at the bottom corners of the character for the groundcheck.
-        public Transform leftFoot, rightFoot;
-        public float moveSpeed = 3000f,
-            jumpPower = 5000f,
-            maxCoyoteTime = .15f,
-            customGravity = 1000f;
+        public LayerMask floorLayer;
 
-        //Graphics related
-        [SerializeField] private SpriteRenderer sr;
-        [SerializeField] private Animator animator;
-        private float _coyoteTime;
-        private bool _isGrounded, _jumpIsQueued;
+        public float moveSpeed = 10f,
+            jumpPower = 30f,
+            jumpTimer = 0.1f,
+            customGravity = 9.88f;
 
-        //Movement & Jumping
+        [SerializeField] private Animator animator = default;
+        [SerializeField] private Transform topLeftBound = default;
+        [SerializeField] private Transform bottomRightBound = default;
+
+
+        private float _coyoteTime, _jumpTime;
+        private PlayerControls _input = default;
+        private bool _isGrounded, _jumping;
+
+
         private Vector2 _moveVector, _currentForceOfGravity;
-        //General Properties
+
         private Rigidbody2D _rigidbody;
+        private SpriteRenderer _spriteRenderer;
+
+        private Collider2D[] _groundCollision = new Collider2D[1];
 
         private void Awake()
         {
-            sr = GetComponent<SpriteRenderer>();
+            _input = new PlayerControls();
+
+            _spriteRenderer = GetComponent<SpriteRenderer>();
             _rigidbody = GetComponent<Rigidbody2D>();
 
-            _rigidbody.gravityScale =
-                0; //We use custom gravity to ensure the smoothest terrain traverlsal possible.
+            //We use custom gravity to ensure the smoothest terrain traversal possible.
+            _rigidbody.gravityScale = 0;
+        }
+
+        private void Start()
+        {
+            _input.Player.Movement.performed += OnMovement;
+            _input.Player.Movement.canceled += _ => _moveVector = Vector2.zero;
+            _input.Player.Primary.started += OnPrimary;
+            _input.Player.Secondary.started += OnSecondary;
         }
 
         private void Update()
         {
             _coyoteTime += Time.deltaTime;
-            ReadControls();
             UpdateSpriteAndAnimations();
         }
+
 
         private void FixedUpdate()
         {
             _isGrounded = GroundCheck();
-            CalcGravity();
+            _currentForceOfGravity = _isGrounded
+                ? Vector2.zero
+                : _currentForceOfGravity + Vector2.down * customGravity;
 
-            if (_jumpIsQueued)
-            {
-                _rigidbody.AddForce(Vector2.up * (jumpPower * Time.fixedDeltaTime),
-                    ForceMode2D.Impulse);
-                _jumpIsQueued = false;
-            }
-
-            _rigidbody.AddForce(
-                (_moveVector + _currentForceOfGravity) * Time.fixedDeltaTime);
+            _rigidbody.AddForce(_currentForceOfGravity +
+                                _moveVector * (moveSpeed * MoveSpeedModifier));
         }
 
-        private void ReadControls()
+        private void OnEnable()
+        {
+            _input.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _input.Disable();
+        }
+
+        public void OnMovement(InputAction.CallbackContext context)
         {
             _moveVector = Vector2.zero;
+            _moveVector.x = context.ReadValue<Vector2>().x;
+        }
 
-            // Movement
-            if (Keyboard.current.leftArrowKey.isPressed)
-            {
-                _moveVector.x = -moveSpeed;
-                if (Keyboard.current.upArrowKey.isPressed) direction = Direction.NW;
-                else if (Keyboard.current.downArrowKey.isPressed)
-                    direction = Direction.SW;
-                else direction = Direction.W;
-            }
-            else if (Keyboard.current.rightArrowKey.isPressed)
-            {
-                _moveVector.x = moveSpeed;
-                if (Keyboard.current.upArrowKey.isPressed) direction = Direction.NE;
-                else if (Keyboard.current.downArrowKey.isPressed)
-                    direction = Direction.SE;
-                else direction = Direction.E;
-            }
+        public void OnPrimary(InputAction.CallbackContext context)
+        {
+            if (context.control.IsPressed()) StartCoroutine(Jump(context.action));
+        }
 
-            if (_moveVector.x != 0 && Keyboard.current.upArrowKey.isPressed)
-                direction = Direction.N;
-            else if (_moveVector.x != 0 && Keyboard.current.downArrowKey.isPressed)
-                direction = Direction.S;
 
-            // A-Button
-            // This is a bool so the input can not be read multiple times before the next physics frame (even if this is very unlikely to happen).
-            if (Keyboard.current.xKey.wasPressedThisFrame && _isGrounded &&
-                !_jumpIsQueued)
-                _jumpIsQueued = true;
-
-            // B-Button
-            if (Keyboard.current.cKey.wasPressedThisFrame)
-                Hook(); //This should live in its own script.
+        public void OnSecondary(InputAction.CallbackContext context)
+        {
+            Hook();
         }
 
         private void Hook()
@@ -103,43 +107,47 @@ namespace Player
             throw new NotImplementedException();
         }
 
+        private IEnumerator Jump(InputAction contextAction)
+        {
+            if (!_isGrounded || _jumping) yield break;
+
+            _jumping = true;
+            _jumpTime = jumpTimer;
+            _rigidbody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+
+            var nextFixedUpdated = new WaitForFixedUpdate();
+            while (_jumping && contextAction.ReadValue<float>() > 0 && _jumpTime > 0)
+            {
+                var prevGrounded = _isGrounded;
+                yield return nextFixedUpdated;
+                _rigidbody.velocity += Vector2.up * jumpPower;
+                _jumpTime -= Time.fixedDeltaTime;
+
+                if (!prevGrounded && _isGrounded)
+                {
+                    Debug.Log("break jump");
+                    break;
+                }
+            }
+
+            _jumping = false;
+        }
+
         private void UpdateSpriteAndAnimations()
         {
-            switch (direction)
-            {
-                case Direction.E:
-                case Direction.NE:
-                case Direction.SE:
-                    sr.flipX = false;
-                    break;
-                case Direction.W:
-                case Direction.NW:
-                case Direction.SW:
-                    sr.flipX = true;
-                    break;
-            }
+            if (_moveVector.x != 0) _spriteRenderer.flipX = !(_moveVector.x >= 0);
 
             //Plug in animation setfloats and setbools here!
         }
 
         private bool GroundCheck()
         {
-            if (Physics2D.Linecast(transform.position, leftFoot.position, floorLayer)
-                || Physics2D.Linecast(transform.position, rightFoot.position,
-                    floorLayer))
-            {
-                _coyoteTime = 0f;
-                return true;
-            }
+            _groundCollision[0] = null;
 
-            return _coyoteTime <= maxCoyoteTime;
-        }
+            Physics2D.OverlapAreaNonAlloc(topLeftBound.position,
+                bottomRightBound.position, _groundCollision, floorLayer);
 
-        private void CalcGravity()
-        {
-            if (!_isGrounded)
-                _currentForceOfGravity.y -= customGravity * Time.fixedDeltaTime;
-            else _currentForceOfGravity = Vector2.zero;
+            return _groundCollision.First();
         }
     }
 }
