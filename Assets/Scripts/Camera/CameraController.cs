8216ffa53fcf32f53f00ab9c25c8fb859f2e53a7
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-08-30 21:33:52 +0100

    Camera improvements

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -3,20 +3,19 @@
 public class CameraController : MonoBehaviour
 {
 	[SerializeField] private Transform _target;
-	[SerializeField] private float _smoothMovementWeight = 0.01f;
+	[SerializeField] private float _smoothMovementMaxSpeed = Mathf.Infinity;
 	[SerializeField] private float _minDistForMovement = 0;
-	[SerializeField] private float _minPosY = 0;
+	[SerializeField] private float _minPosY = Mathf.NegativeInfinity;
+	[SerializeField] private float _maxPosY = Mathf.Infinity;
+	[SerializeField] private float _targetOffsetY = 0;
 
-    void Update()
-    {
-		if (Mathf.Abs(_target.position.y - transform.position.y) > _minDistForMovement)
-		{
-			transform.position = Vector3.Lerp(transform.position,
-				new Vector3(
-				transform.position.x,
-				Mathf.Max(_target.position.y, _minPosY),
-				transform.position.z),
-				_smoothMovementWeight);
-		}
-    }
+	private Vector3 _velocity;
+
+	private void FixedUpdate()
+	{
+		Vector3 targetPos = new Vector3(transform.position.x, _target.position.y + _targetOffsetY, transform.position.z);
+		targetPos = new Vector3(targetPos.x, Mathf.Clamp(targetPos.y, _minPosY, _maxPosY), targetPos.z);
+
+		transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, default);
+	}
 }
