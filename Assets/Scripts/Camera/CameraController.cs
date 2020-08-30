8e3e16d7d934d14d5604739771364fd7143446fd
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-08-30 21:45:27 +0100

    Tweaking

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,7 +4,6 @@ public class CameraController : MonoBehaviour
 {
 	[SerializeField] private Transform _target;
 	[SerializeField] private float _smoothMovementMaxSpeed = Mathf.Infinity;
-	[SerializeField] private float _minDistForMovement = 0;
 	[SerializeField] private float _minPosY = Mathf.NegativeInfinity;
 	[SerializeField] private float _maxPosY = Mathf.Infinity;
 	[SerializeField] private float _targetOffsetY = 0;
@@ -16,6 +15,6 @@ public class CameraController : MonoBehaviour
 		Vector3 targetPos = new Vector3(transform.position.x, _target.position.y + _targetOffsetY, transform.position.z);
 		targetPos = new Vector3(targetPos.x, Mathf.Clamp(targetPos.y, _minPosY, _maxPosY), targetPos.z);
 
-		transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, default);
+		transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, default, _smoothMovementMaxSpeed);
 	}
 }
