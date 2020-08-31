16a5eeb907ff6645caaa5d5b70dbc9c11e4732c9
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-08-31 15:24:58 +0100

    Entity Health and HUD Health icons

---
+++ Assets/Scripts/EntityHealth.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+class EntityHealth : MonoBehaviour
+{
+	[SerializeField] private int _health;
+	public int Health
+	{
+		protected set
+		{
+			_health = value;
+			OnHealthSet(value);
+		}
+		get
+		{
+			return _health;
+		}
+	}
+
+	public delegate void HealthSetEventHandler(int health);
+	public event HealthSetEventHandler HealthSetEvent;
+
+	public delegate void TakeDamageEventHandler(int damage);
+	public event TakeDamageEventHandler TakeDamageEvent;
+
+	protected virtual void OnHealthSet(int health)
+	{
+		HealthSetEvent?.Invoke(health);
+	}
+
+	protected virtual void OnTakeDamage(int damage)
+	{
+		TakeDamageEvent?.Invoke(damage);
+	}
+
+	private void Start()
+	{
+		OnHealthSet(Health);
+	}
+
+	public virtual void TakeDamage(int damage)
+	{
+		Health -= damage;
+		OnTakeDamage(damage);
+	}
+}
---
+++ Assets/Scripts/HUD/HUDHealthHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class HUDHealthHandler : MonoBehaviour
+{
+	[SerializeField] private EntityHealth _targetEntityHealth;
+	[SerializeField] private GameObject _healthImagePrefab;
+	[SerializeField] private int _healthImagePadding;
+	[SerializeField] private int _healthImageTopStartPadding;
+
+    private void Start()
+    {
+		_targetEntityHealth.HealthSetEvent += SetHUDHealth;
+	}
+
+	private void SetHUDHealth(int health)
+	{
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			Destroy(transform.GetChild(i).gameObject);
+		}
+
+		int numHealthImages = health - transform.childCount;
+
+		for (int i = 0; i < numHealthImages; i++)
+		{
+			GameObject heartImage = Instantiate(_healthImagePrefab);
+			heartImage.transform.SetParent(transform);
+
+			RectTransform heartImageRectTransform = heartImage.GetComponent<RectTransform>();
+			heartImageRectTransform.anchoredPosition = new Vector3(
+				-heartImageRectTransform.rect.width / 2,
+				-heartImageRectTransform.rect.height / 2 + _healthImageTopStartPadding - (heartImageRectTransform.rect.height + _healthImagePadding) * i,
+				0);
+			heartImageRectTransform.localScale = Vector3.one;
+		}
+	}
+}
