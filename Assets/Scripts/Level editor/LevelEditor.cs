2f183ed4c132a035affa52df9c30d37b9a5addc8
unsettledgames <47360416+unsettledgames@users.noreply.github.com>
2020-08-31 19:04:53 +0200

    Started paiting function

---
+++ Assets/Scripts/Level editor/LevelEditor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+public class LevelEditor : MonoBehaviour
+{
+    public enum Tools {Pencil, Eraser, Fill, Select, Move}
+    public static LevelEditor Instance;
+    public Tilemap tilemap;
+    public GameObject testObject;
+
+    // Using private members so that update isn't too expensive
+    private Vector3 mousePosition;
+    private Vector3 mouseWorldPosition;
+    private Vector3 mouseTilemapPosition;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mousePosition = new Vector3();
+        mouseWorldPosition = new Vector3();
+        mouseTilemapPosition = new Vector3();
+
+        // Freezing time so that the user can create the level
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Mouse.current.position.ReadValue();
+        mousePosition.z = FrequentlyAccessed.Instance.camera.nearClipPlane;
+
+        mouseWorldPosition = FrequentlyAccessed.Instance.camera.ScreenToWorldPoint(mousePosition);
+        mouseTilemapPosition = tilemap.WorldToCell(mouseWorldPosition / 8);
+
+        testObject.transform.position = mouseWorldPosition;
+        Debug.Log("Tile mouse: " + mouseWorldPosition);
+    }
+}
