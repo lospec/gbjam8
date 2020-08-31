f0b21012d4173d9b6e782a11304ebf91f622f179
Pillsgood <arashm35@gmail.com>
2020-08-31 23:11:55 +0800

    Add Random Enemy Spawning;

--- Assets/Scripts/RoomLoading/RoomData.cs
+++ Assets/Scripts/RoomLoading/RoomData.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 using Utility;
 
@@ -18,8 +19,17 @@ namespace RoomLoading
         [InspectorReadOnly] public int maxY;
         [InspectorReadOnly] public int height = -1;
 
+        private bool _readHeight = false;
+        
         public int GetRoomHeight()
         {
+            if (_readHeight)
+            {
+                return height;
+            }
+
+            _readHeight = true;
+
             // Cellbounds might not be up to date until CompressBounds is called
             tilemap.CompressBounds();
             borderBlender.CompressBounds();
