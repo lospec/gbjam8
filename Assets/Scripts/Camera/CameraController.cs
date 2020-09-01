c851a290a5d8c220764db27cc73ecaebece58c1c
Pillsgood <arashm35@gmail.com>
2020-09-02 00:46:15 +0800

    add background parallax;

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,20 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-	[SerializeField] private Transform _target;
-	[SerializeField] private float _smoothMovementMaxSpeed = Mathf.Infinity;
-	[SerializeField] private float _minPosY = Mathf.NegativeInfinity;
-	[SerializeField] private float _maxPosY = Mathf.Infinity;
-	[SerializeField] private float _targetOffsetY = 0;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _smoothMovementMaxSpeed = Mathf.Infinity;
+    [SerializeField] private float _minPosY = Mathf.NegativeInfinity;
+    [SerializeField] private float _maxPosY = Mathf.Infinity;
+    [SerializeField] private float _targetOffsetY = 0;
 
-	private Vector3 _velocity;
+    private Vector3 _velocity;
+    public Vector2 Velocity => _velocity;
 
-	private void LateUpdate()
-	{
-		Vector3 targetPos = new Vector3(transform.position.x, _target.position.y + _targetOffsetY, transform.position.z);
-		targetPos = new Vector3(targetPos.x, Mathf.Clamp(targetPos.y, _minPosY, _maxPosY), targetPos.z);
+    private void LateUpdate()
+    {
+        Vector3 targetPos = new Vector3(transform.position.x,
+            _target.position.y + _targetOffsetY, transform.position.z);
+        targetPos = new Vector3(targetPos.x,
+            Mathf.Clamp(targetPos.y, _minPosY, _maxPosY), targetPos.z);
 
-		transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, default, _smoothMovementMaxSpeed);
-	}
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos,
+            ref _velocity, default, _smoothMovementMaxSpeed);
+    }
 }
