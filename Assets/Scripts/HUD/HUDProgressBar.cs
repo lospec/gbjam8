483de4f65a88f5fd6ea7bdd3102ee36151c88578
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-09-01 10:11:12 +0100

    Progress Bar (Currently based on player position/distance to goal)

---
+++ Assets/Scripts/HUD/HUDProgressBar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDProgressBar : MonoBehaviour
+{
+	[SerializeField] private Player.CharacterController _player;
+	[SerializeField] private float _minPlayerPosY;
+	[SerializeField] private float _maxPlayerPosY;
+	[SerializeField] private Slider _slider;
+
+	private void Update()
+	{
+		float value = Mathf.InverseLerp(_minPlayerPosY, _maxPlayerPosY, _player.transform.position.y);
+		_slider.value = value;
+	}
+}
