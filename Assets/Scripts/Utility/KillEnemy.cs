a4d90020bbb4d694fa37d42c611ca2382468d5e7
Pillsgood <arashm35@gmail.com>
2020-09-05 02:13:06 +0800

    add enemy death gfx;

---
+++ Assets/Scripts/Utility/KillEnemy.cs
@@ -0,0 +1,30 @@
+using Enemy;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Utility
+{
+    // SCRIPT IS FOR TESTING! DO NOT USE FOR RELEASE BUILD
+    public class KillEnemy : MonoBehaviour
+    {
+        [SerializeField] private EnemyController enemyController;
+        [SerializeField] private GameObject explosion;
+
+        private void Start()
+        {
+            (enemyController as FlyingEnemyController).Initialize(null,
+                FlyingEnemyController.StartState.Flying);
+        }
+
+        private void Update()
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                var dissolve = enemyController.GetComponentInChildren<Dissolve>();
+                dissolve.enabled = true;
+                Instantiate(explosion, enemyController.transform.position,
+                    Quaternion.identity);
+            }
+        }
+    }
+}
