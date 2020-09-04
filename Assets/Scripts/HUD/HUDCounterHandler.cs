4ea003a1c4866c00b154c75d38de9548c1504ae7
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-09-04 16:48:56 +0100

    HUD Ammo Counter
    - Removed old HUD progress bar and its corresponding sprites
    - Added ammo sprite
    - Change HUDHealthHandler to HUDCounterHandler and renamed variables where appropriate
    - Modified EntityHealth to invoke an OnHealthSet event when health is set and at the start of the script to allow for the HUD callback methods to be connected
    - Added the AmmoCounter game object to the HUD prefab (found in the HUDPreview scene) which has a HUDCounterHandler script component

---
+++ Assets/Scripts/HUD/HUDCounterHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class HUDCounterHandler : MonoBehaviour
+{
+	[SerializeField] private GameObject _imagePrefab;
+	[SerializeField] private int _imagePadding;
+	[SerializeField] private int _imageTopPadding;
+
+	public void SetHUDCounter(int value)
+	{
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			Destroy(transform.GetChild(i).gameObject);
+		}
+
+		int numImages = value - transform.childCount;
+
+		for (int i = 0; i < numImages; i++)
+		{
+			GameObject image = Instantiate(_imagePrefab);
+			image.transform.SetParent(transform);
+
+			RectTransform imageRectTransform = image.GetComponent<RectTransform>();
+			imageRectTransform.anchoredPosition = new Vector3(
+				-imageRectTransform.rect.width / 2,
+				-imageRectTransform.rect.height / 2 - _imageTopPadding - (imageRectTransform.rect.height + _imagePadding) * i,
+				0);
+			imageRectTransform.localScale = Vector3.one;
+		}
+	}
+}
