d4194e1591d8f7bc4bf4ed2a9ae4b321169b1824
Pillsgood <arashm35@gmail.com>
2020-09-05 20:23:28 +0800

    remove redundant code in KillEnemy.cs;

--- Assets/Scripts/Utility/KillEnemy.cs
+++ Assets/Scripts/Utility/KillEnemy.cs
@@ -4,19 +4,15 @@ using UnityEngine.InputSystem;
 
 namespace Utility
 {
-    // SCRIPT IS FOR TESTING! DO NOT USE FOR RELEASE BUILD
     public class KillEnemy : MonoBehaviour
     {
-        [SerializeField] private EnemyController enemyController;
         [SerializeField] private Dissolve enemyDissolve;
-        
         [SerializeField] private GameObject explosion;
-        
+
         public void Explode()
         {
             enemyDissolve.enabled = true;
-            Instantiate(explosion, enemyController.transform.position,
-                Quaternion.identity);
+            Instantiate(explosion, transform.position, Quaternion.identity);
         }
     }
 }
