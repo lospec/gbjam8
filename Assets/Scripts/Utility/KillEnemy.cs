2c0177a347bb3f8940a32f671d356572853a848d
Pillsgood <arashm35@gmail.com>
2020-09-06 06:14:40 +0800

    update KillEnemy.cs;

--- Assets/Scripts/Utility/KillEnemy.cs
+++ Assets/Scripts/Utility/KillEnemy.cs
@@ -4,18 +4,15 @@ using UnityEngine.InputSystem;
 
 namespace Utility
 {
-    // SCRIPT IS FOR TESTING! DO NOT USE FOR RELEASE BUILD
     public class KillEnemy : MonoBehaviour
     {
-        [SerializeField] private EnemyController enemyController;
+        [SerializeField] private Dissolve enemyDissolve;
         [SerializeField] private GameObject explosion;
 
         public void Explode()
         {
-            var dissolve = GetComponentInChildren<Dissolve>();
-            dissolve.enabled = true;
-            Instantiate(explosion, transform.position,
-                Quaternion.identity);
+            enemyDissolve.enabled = true;
+            Instantiate(explosion, transform.position, Quaternion.identity);
         }
     }
 }
