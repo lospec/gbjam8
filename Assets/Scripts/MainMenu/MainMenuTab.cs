980a4171358607fba25779d99091eac97bddffd7
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-09-05 19:52:33 +0100

    Controls and Credits

---
+++ Assets/Scripts/MainMenu/MainMenuTab.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MainMenuTab : MonoBehaviour
+{
+	private void Update()
+	{
+		if (new[]
+		{
+			Keyboard.current.anyKey
+		}.Any(control => control.wasPressedThisFrame))
+		{
+			GoToMainMenuScene();
+		}
+	}
+
+	public void GoToMainMenuScene()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+}
