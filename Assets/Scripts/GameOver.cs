bfbf3c0dd5f73aed3e0567fc530184ef1260a175
Pillsgood <arashm35@gmail.com>
2020-09-06 21:38:29 +0800

    Set inactive on validate;

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,24 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-	private void Awake()
-	{
-		gameObject.SetActive(false);
-	}
+    private void Update()
+    {
+        if (Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            Restart();
+        }
+    }
 
-	private void Update()
-	{
-		if (Keyboard.current.enterKey.wasPressedThisFrame)
-		{
-			Restart();
-		}
-	}
+    private void OnValidate()
+    {
+        gameObject.SetActive(false);
+    }
 
-	public void Restart()
-	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-	}
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
