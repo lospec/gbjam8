233d3903dd5355afdb2b36c466f42e0383990299
mariothedog <28541089+mariothedog@users.noreply.github.com>
2020-09-06 20:14:36 +0100

    Fix build errors

--- Assets/Scripts/Utility/EventValidator.cs
+++ Assets/Scripts/Utility/EventValidator.cs
@@ -1,4 +1,6 @@
+#if UNITY_EDITOR
 using UnityEditor.Events;
+#endif
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,7 +8,8 @@ namespace Utility
 {
     public static class EventValidator
     {
-        private static void ConnectEvent<T>(UnityEvent<T> @event, UnityAction<T> action)
+#if UNITY_EDITOR
+		private static void ConnectEvent<T>(UnityEvent<T> @event, UnityAction<T> action)
         {
             for (var i = 0; i < @event.GetPersistentEventCount(); i++)
             {
@@ -20,19 +23,22 @@ namespace Utility
 
             UnityEventTools.AddPersistentListener(@event, action);
         }
+#endif
 
-        public static void ValidatePlayerHud(HudManager hud, GameObject player)
+		public static void ValidatePlayerHud(HudManager hud, GameObject player)
         {
             if (!hud || !player)
             {
                 return;
             }
 
-            ConnectEvent(player.GetComponent<PlayerHealth>().OnHealthSet,
+#if UNITY_EDITOR
+			ConnectEvent(player.GetComponent<PlayerHealth>().OnHealthSet,
                 hud.HealthBar.SetHUDHealth);
             ConnectEvent(
                 player.GetComponent<EntityScore>().OnScoreSet,
                 hud.Score.SetHUDScore);
-        }
+#endif
+		}
     }
 }
